2194b6a4b060587422224abadabf7f62556c2879
venomXbots <152503093+venomXbots@users.noreply.github.com>
2023-12-05 07:03:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Ayushjha9988/KunoMusic",
+    "https://github.com/venomXbots/Lilyrobot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
